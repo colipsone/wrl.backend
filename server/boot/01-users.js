d68c031f615d85fbe8697b3e06a72e49a1627b08
colipsone@gmail.com <900ltdzyjcnj9>
2016-02-08 07:05:20 +0200

    Added basic roles and assigning them to admin users during app boot. Added ACL restrictions to apartment model.

--- server/boot/01-users.js
+++ server/boot/01-users.js
@@ -3,25 +3,37 @@
  */
 module.exports = function (app) {
 
-  createUsers();
+  // create default user
+  createDefaultUsers();
 
-  function createUsers() {
-
+  function createDefaultUsers() {
     var Agent = app.models.agent;
 
-    var users = getDefaultUsers();
-
-    users.forEach(function(agent){
-      Agent.findOrCreate({email: agent.email}, agent, function(err, instance){
-        console.log(instance);
-      });
-    });
-  }
-
-  function getDefaultUsers(){
-    return [{
+    var admins = [{
       email: 'xim@rambler.ru',
       password: 'xim'
     }];
+
+    admins.forEach(function (admin) {
+      console.log(Agent.findOrCreate({email: admin.email}, function (err, dbAgent) {
+        if (err) throw err;
+        console.log('Admin: \'' + dbAgent.email + '\' has been created.');
+        assignUserToRole('admin', dbAgent);
+        assignUserToRole('agent', dbAgent);
+      }))
+    });
+  }
+
+  function assignUserToRole(roleName, user) {
+    var Role = app.models.Role;
+    Role.findOrCreate({name: roleName}, function (err, dbRole) {
+      if (err) throw err;
+      //assign user to the role
+      var RoleMapping = app.models.RoleMapping;
+      RoleMapping.findOrCreate({roleId: dbRole.id, principalId: user.id}, function (err) {
+        if (err) throw err;
+        console.log('\'' + dbRole.name + '\' role has been assigned to \'' + user.email + '\' user.');
+      });
+    })
   }
 }
